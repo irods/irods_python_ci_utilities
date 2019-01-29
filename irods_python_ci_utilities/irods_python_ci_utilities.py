da9faa930d54dcf2922c6e82a38547c6ef9be441
Terrell Russell <terrellrussell@gmail.com>
2019-01-29 13:13:03 -0500

    only install non-empty packages list

--- irods_python_ci_utilities/irods_python_ci_utilities.py
+++ irods_python_ci_utilities/irods_python_ci_utilities.py
@@ -49,30 +49,34 @@ stderr: {4}
     return p.returncode, out, err
 
 def install_os_packages_apt(packages):
-    subprocess_get_output(['sudo', 'apt-get', 'clean'], check_rc=True)
-    subprocess_get_output(['sudo', 'apt-get', 'update'], check_rc=True)
-    args = ['sudo', 'apt-get', 'install', '-y'] + list(packages)
-    subprocess_get_output(args, check_rc=True)
+    if packages:
+        subprocess_get_output(['sudo', 'apt-get', 'clean'], check_rc=True)
+        subprocess_get_output(['sudo', 'apt-get', 'update'], check_rc=True)
+        args = ['sudo', 'apt-get', 'install', '-y'] + list(packages)
+        subprocess_get_output(args, check_rc=True)
 
 def install_os_packages_yum(packages):
-    args = ['sudo', 'yum', 'install', '-y'] + list(packages)
-    subprocess_get_output(args, check_rc=True)
+    if packages:
+        args = ['sudo', 'yum', 'install', '-y'] + list(packages)
+        subprocess_get_output(args, check_rc=True)
 
 def install_os_packages_zypper(packages):
-    args = ['sudo', 'zypper', '--non-interactive', 'install'] + list(packages)
-    subprocess_get_output(args, check_rc=True)
+    if packages:
+        args = ['sudo', 'zypper', '--non-interactive', 'install'] + list(packages)
+        subprocess_get_output(args, check_rc=True)
 
 def install_os_packages(packages):
-    dispatch_map = {
-        'Ubuntu': install_os_packages_apt,
-        'Centos': install_os_packages_yum,
-        'Centos linux': install_os_packages_yum,
-        'Opensuse ': install_os_packages_zypper,
-    }
-    try:
-        dispatch_map[copied_from_ansible.get_distribution()](packages)
-    except KeyError:
-        raise_not_implemented_for_distribution()
+    if packages:
+        dispatch_map = {
+            'Ubuntu': install_os_packages_apt,
+            'Centos': install_os_packages_yum,
+            'Centos linux': install_os_packages_yum,
+            'Opensuse ': install_os_packages_zypper,
+        }
+        try:
+            dispatch_map[copied_from_ansible.get_distribution()](packages)
+        except KeyError:
+            raise_not_implemented_for_distribution()
 
 def install_os_packages_from_files_apt(files):
     '''files are installed individually in the order supplied, so inter-file dependencies must be handled by the caller'''
